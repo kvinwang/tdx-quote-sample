21adb7d09e0b83c4ce06a519befe2203b14882d5
Kevin Wang <wy721@qq.com>
2024-10-16 05:22:13 +0000

    Add verification

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use base64::prelude::*;
 use clap::Parser;
-use scale::Encode;
+use scale::{Decode, Encode};
 use serde::Deserialize;
-use std::error::Error;
+use std::{error::Error, time::SystemTime};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -42,9 +42,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("Quote response: {:?}", quote_response);
 
     let quote_bin = BASE64_STANDARD.decode(&quote_response.quote)?;
-    std::fs::write(&args.quote, &quote_bin)?;
-    println!("Quote written to {}", args.quote);
-
     let collateral_response = dcap_qvl::collateral::get_collateral(
         &args.pccs,
         &quote_bin,
@@ -52,8 +49,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
     )
     .await?;
     println!("Collateral response: {:?}", collateral_response);
+    std::fs::write(&args.quote, &quote_bin)?;
+    println!("Quote written to {}", args.quote);
     let collateral_bin = collateral_response.encode();
-    std::fs::write(&args.collateral, collateral_bin)?;
+    std::fs::write(&args.collateral, &collateral_bin)?;
     println!("Collateral written to {}", args.collateral);
+    verify_quote(&args.quote, &args.collateral)?;
+    Ok(())
+}
+
+fn verify_quote(quote_file: &str, collateral_file: &str) -> Result<(), Box<dyn Error>> {
+    let quote = std::fs::read(quote_file)?;
+    let collateral = std::fs::read(collateral_file)?;
+    let now = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_secs();
+    let collateral = dcap_qvl::QuoteCollateralV3::decode(&mut &collateral[..])?;
+    let _verified =
+        dcap_qvl::verify::verify(&quote, &collateral, now).expect("Failed to verify quote");
+    println!("Verified");
     Ok(())
 }
